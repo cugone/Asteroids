12bbc129489724cb56993d445aa593e0014f3844
cugone <cugone@gmail.com>
2021-08-20 09:50:41 -0500

    Removed unused headers

--- Asteroids/Code/Game/Main_Win32.cpp
+++ Asteroids/Code/Game/Main_Win32.cpp
@@ -1,25 +1,10 @@
 
-#include "Engine/Core/BuildConfig.hpp"
-#include "Engine/Core/Config.hpp"
-#include "Engine/Core/Console.hpp"
 #include "Engine/Core/EngineBase.hpp"
-#include "Engine/Core/EngineCommon.hpp"
-#include "Engine/Core/FileLogger.hpp"
-#include "Engine/Core/KeyValueParser.hpp"
+
 #include "Engine/Core/StringUtils.hpp"
 #include "Engine/Core/Win.hpp"
 
-#include "Engine/Memory/MemoryPool.hpp"
-
-#include "Engine/Renderer/Renderer.hpp"
-#include "Engine/Renderer/Window.hpp"
-
 #include "Game/Game.hpp"
-#include "Game/GameCommon.hpp"
-
-#include <sstream>
-#include <memory>
-#include <vector>
 
 #pragma warning(push)
 #pragma warning(disable: 28251)
